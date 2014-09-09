ed1672068f13337b048b6bb9ae9851227f8f3106
Markus <markus.joemets@khk.ee>
2014-09-09 15:08:46 +0300

    Add sample variables

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title> harjutus-3 </title>
+</head>
+<body>
+<h1> Muutujad </h1>
+<?php
+    $variable = 5;
+    echo $variable;
+    // Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+    echo "<br>";
+?>
+<?php
+    $variable = 10;
+    echo $variable;
+?>
+
+
+
+
+</body>
+</html>
